5a80c89502bc3a503c4a24d6d8ad650bf43a3706
abhi <abhijeetkc01@gmail.com>
2024-05-04 00:41:15 +0545

    localstorage json for saving todos

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 //Model
-let todos = [{
-    title: 'write code',
-    due_date: '2024-01-30',
-    id: 'id1'
-},{
-    title: 'stay focussed',
-    due_date: '2024-02-30',
-    id: 'id2'
-},{
-    title: 'believe in yourself',
-    due_date: '2024-03-30',
-    id: 'id3'
-}]
-todos.push({
-    title: 'keep learning',
-    due_date: '2024-04-30',
-    id: 'id4'
-});
+let todos;
 
+const savedTodos = JSON.parse(localStorage.getItem('todos'));
+
+if(Array.isArray(savedTodos)){
+    todos = savedTodos;
+    console.log("todos",todos);
+    console.log("savedTodos",savedTodos);
+}
+
+else {
+    let todos = [{
+        title: 'write code',
+        due_date: '2024-01-30',
+        id: 'id1'
+    }, {
+        title: 'stay focussed',
+        due_date: '2024-02-30',
+        id: 'id2'
+    }, {
+        title: 'believe in yourself',
+        due_date: '2024-03-30',
+        id: 'id3'
+    }]
+    todos.push({
+        title: 'keep learning',
+        due_date: '2024-04-30',
+        id: 'id4'
+    });
+}
 //creates todo
 
-function createTodo(text, due_date){
+function createTodo(text, due_date) {
 
     const id = new Date().getTime().toString();   // converting number to string
 
     // create new todo card only if all filed are filled
-    if(text && due_date){ 
+    if (text && due_date) {
         todos.push(
             {
                 title: text,
@@ -34,41 +45,52 @@ function createTodo(text, due_date){
             }
         );
     }
+
+    else {
+        alert("Please fill all the fields");
+    }
+
+    saveTodos();
 }
 
 //removes todo
-function removeTodo(idToDelete){
+function removeTodo(idToDelete) {
 
     todos = todos.filter(function (todo) {
         // return all todos except the one with the id to delete
         return todo.id !== idToDelete;
     });
 
+    saveTodos();
+
 }
 
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 
 //Controller
 function addTodo() {
 
     const text_input = document.getElementById('todo');
     const text = text_input.value;
-    
+
     const datePicker = document.getElementById('DatePicker');
     const due_date = datePicker.value;
-    
-    createTodo(text, due_date);    
-    
+
+    createTodo(text, due_date);
+
     render();
-    
+
 }
 
-function deleteTodo(event){
+function deleteTodo(event) {
 
     const deleteButton = event.target;
     const idToDelete = deleteButton.id;
-    
+
     removeTodo(idToDelete);
-    
+
     render();
 }
 
@@ -79,7 +101,7 @@ function render() {
 
     todos.forEach(function (todo) {
         const element = document.createElement('div');
-        
+
         const title = document.createElement('p');
         title.innerHTML = todo.title + ' | ' + todo.due_date;
         title.classList.add("todo-title"); // adding class name to title for css
@@ -97,12 +119,14 @@ function render() {
         const todolist = document.getElementById('todos-list');
         todolist.appendChild(element);
     });
+
     const todolist = document.getElementById('todos-list');
     const title = document.createElement('p');
     title.classList.add("no-todo-text");
     title.innerHTML = "No todos ";
-    if(todos.length == 0){
+    if (todos.length == 0) {
         todolist.appendChild(title);
     }
+
 }
 render();
